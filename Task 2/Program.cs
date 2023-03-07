c35da587150e7643cf272e49ad9fc9c52fa718b3
Alexander <fiireex@yandex.ru>
2023-03-07 21:29:04 +0300

    Complete Task 2

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,2 +1,19 @@
 ﻿Console.Clear();
 Console.Write("Введите первое число ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число ");
+int с = int.Parse(Console.ReadLine()!);
+
+int max = 0;
+
+if(max < a)
+    max = a;
+if(max < b)
+    max = b;
+if(max < с)
+    max = с;
+Console.Write("Максимальное число = ");
+Console.Write(max);
+
